270a14b76beec532173e9807a5d5021010727686
Bilal Hussain <b.hussain@dnata.com>
2020-04-05 01:19:20 +0400

    implemented useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class App extends Component {
+const App = () => {
+  const [resource, setResource] = useState('posts');
 
-    state = {
-        resource: 'posts'
-    }
-
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState({ resource: 'posts'})}>Posts</button>
-          <button onClick={() => this.setState({ resource: 'todos' })} >Todos</button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource('posts')}>
+          Posts
+        </button>
+        <button onClick={() => setResource('todos')}>
+          Todos
+        </button>
       </div>
-    );
-  }
-}
-
+      {resource}
+    </div>
+  );
+};
 
 export default App;
